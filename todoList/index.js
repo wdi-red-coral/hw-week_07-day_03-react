f36333b4c63c26263c914a721cd3bf6cc4860417
AmanyAts <amanyalattass@gmail.com>
2019-06-26 10:10:18 +0300

    Teusday HW

---
+++ todoList/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Bob Ziroll</h1>
+        <p>This is a paragraph about me...</p>
+        <ul>
+          <li>Thailand</li>
+          <li>Japan</li>
+          <li>Nordic Countries</li>
+        </ul>
+      </div>
+    )
+  }
+  ReactDOM.render(<MyInfo />, document.getElementById("root"))
