9aa32ba7a7b0aae0872a1470545e7c8919ad76b6
AmanyAts <amanyalattass@gmail.com>
2019-06-27 00:00:13 +0300

    Wednesday HW

--- todoList/index.js
+++ todoList/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom"
 
 
 
-function MyInfo() {
-    return (
-      <div>
-        <h1>Bob Ziroll</h1>
-        <p>This is a paragraph about me...</p>
-        <ul>
-          <li>Thailand</li>
-          <li>Japan</li>
-          <li>Nordic Countries</li>
-        </ul>
-      </div>
-    )
-  }
+// function MyInfo() {
+//     return (
+//       <div>
+//         <h1>Bob Ziroll</h1>
+//         <p>This is a paragraph about me...</p>
+//         <ul>
+//           <li>Thailand</li>
+//           <li>Japan</li>
+//           <li>Nordic Countries</li>
+//         </ul>
+//       </div>
+//     )
+//   }
   ReactDOM.render(<MyInfo />, document.getElementById("root"))
