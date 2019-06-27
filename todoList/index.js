84c6cc8b9fdf5cd83a643bccb3e054ca58eb62d4
somayahbahkeem <iove36@hotmail.com>
2019-06-27 07:59:22 +0300

    Wednesday HW

--- todoList/index.js
+++ todoList/index.js
@@ -1,8 +1,6 @@
-
-
 import React from "react"
 import ReactDOM from "react-dom"
 
-import MyInfo from "./components/MyInfo"
+import App from "./App"
 
-ReactDOM.render(<MyInfo />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
