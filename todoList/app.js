843171c7201361760da20d4fbfe7d945dccf9f35
ksaninja <salha.aqeel@gmail.com>
2019-06-30 03:47:20 +0300

    thursday

---
+++ todoList/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+        }}
+        render() {
+            return (
+                <div>
+                    <h1>{this.state.name}</h1>
+                    <h3>{this.state.age} years old</h3>
+            </div>)}}
